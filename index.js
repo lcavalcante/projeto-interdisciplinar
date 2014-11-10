936b74e1e508bd8b8609b5d42c093d57f6eec516
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2014-11-10 16:17:10 -0300

    introduzindo websockets

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("Hello World!!!");
-})
+  response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
