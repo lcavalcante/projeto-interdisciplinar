304d114bc445062bd8300955cc107f2f75d30cd9
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2014-11-11 18:45:17 -0300

    #$%*

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
+var paper = require('paper');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -9,13 +10,13 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
-
+/*
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-
+*/
 http.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
